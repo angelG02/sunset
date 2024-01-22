d4e35414fe63a5424617899ea6d2ff944b99d33c
Angel Georgiev <210798@buas.nl>
2024-01-22 16:00:49 +0100

    added instanced pentagrams

--- src/renderer/sun.rs
+++ src/renderer/sun.rs
@@ -1,8 +1,10 @@
 use std::{collections::HashMap, sync::Arc};
 
+use cgmath::prelude::*;
+
 use async_trait::async_trait;
 use tracing::info;
-use wgpu::BufferUsages;
+use wgpu::{util::DeviceExt, BufferUsages};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoopProxy,
@@ -45,10 +47,80 @@ pub struct PipelineDesc {
 
 use super::{
     buffer::{CameraUniform, SunBuffer},
-    pipeline::SunPipeline,
-    primitive::Primitive,
+    pipeline::{self, SunPipeline},
+    primitive::{Primitive, Vertex},
     texture::GPUTexture,
 };
+
+struct Instance {
+    position: cgmath::Vector3<f32>,
+    rotation: cgmath::Quaternion<f32>,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct InstanceRaw {
+    model: [[f32; 4]; 4],
+}
+
+impl InstanceRaw {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,
+            // We need to switch from using a step mode of Vertex to Instance
+            // This means that our shaders will only change to use the next
+            // instance when the shader starts processing a new instance
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: &[
+                // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
+                // for each vec4. We'll have to reassemble the mat4 in the shader.
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    // While our vertex shader only uses locations 0, and 1 now, in later tutorials, we'll
+                    // be using 2, 3, and 4, for Vertex. We'll start at slot 5, not conflict with them later
+                    shader_location: 5,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+                    shader_location: 6,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+                    shader_location: 7,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+                    shader_location: 8,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+            ],
+        }
+    }
+}
+
+unsafe impl bytemuck::Pod for InstanceRaw {}
+unsafe impl bytemuck::Zeroable for InstanceRaw {}
+
+impl Instance {
+    fn to_raw(&self) -> InstanceRaw {
+        InstanceRaw {
+            model: (cgmath::Matrix4::from_translation(self.position)
+                * cgmath::Matrix4::from(self.rotation))
+            .into(),
+        }
+    }
+}
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+    0.0,
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+);
 
 pub struct Sun {
     instance: Option<wgpu::Instance>,
@@ -71,6 +143,9 @@ pub struct Sun {
     // This needs to be querried from the scene
     pub current_camera: Option<CameraComponent>,
     pub current_camera_buffer: Option<SunBuffer>,
+
+    instances: Vec<Instance>,
+    instance_buffer: Option<wgpu::Buffer>,
 
     pub lined: bool,
 
@@ -133,6 +208,20 @@ impl Sun {
         }
         if self.device.is_none() {
             self.create_device().await;
+
+            let instance_data = self
+                .instances
+                .iter()
+                .map(Instance::to_raw)
+                .collect::<Vec<_>>();
+            let instance_buffer = self.device.as_ref().unwrap().create_buffer_init(
+                &wgpu::util::BufferInitDescriptor {
+                    label: Some("Instance Buffer"),
+                    contents: bytemuck::cast_slice(&instance_data),
+                    usage: wgpu::BufferUsages::VERTEX,
+                },
+            );
+            self.instance_buffer = Some(instance_buffer);
         }
 
         let vp = vp_desc.build(
@@ -163,7 +252,7 @@ impl Sun {
             topology,
         );
 
-        if self.current_camera.is_none() && name == "basic_shader.wgsl" {
+        if self.current_camera.is_none() {
             let camera = CameraComponent {
                 camera_type: CamType::Perspective(PerspectiveProps {
                     aspect: viewport.config.width as f32 / viewport.config.height as f32,
@@ -191,20 +280,24 @@ impl Sun {
                 bytemuck::cast_slice(&[camera_uniform]),
                 self.device.as_ref().unwrap(),
             );
-
-            pipeline.add_bind_group(
-                self.device.as_ref().unwrap(),
-                camera.uuid,
-                "camera",
-                1,
-                &[wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: camera_buffer.get_buffer().as_entire_binding(),
-                }],
-            );
             self.current_camera = Some(camera);
             self.current_camera_buffer = Some(camera_buffer);
         }
+        pipeline.add_bind_group(
+            self.device.as_ref().unwrap(),
+            self.current_camera.as_ref().unwrap().uuid,
+            "camera",
+            1,
+            &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: self
+                    .current_camera_buffer
+                    .as_ref()
+                    .unwrap()
+                    .get_buffer()
+                    .as_entire_binding(),
+            }],
+        );
         self.pipelines.insert(name, pipeline);
 
         if !state::initialized() {
@@ -290,30 +383,32 @@ impl Sun {
                 });
 
                 for primitive in &render_desc.primitives {
-                    if let Some((name, pipeline)) = pipeline_entry {
+                    if let Some((_, pipeline)) = pipeline_entry {
                         rpass.set_pipeline(&pipeline.pipeline);
 
                         if let Some(cam) = &self.current_camera {
-                            if name != "line_shader.wgsl" {
-                                // Diffuse texture bind group (set to default missing texture if not present on the primitive)
-                                if let Some(tex_name) = &primitive.temp_diffuse {
-                                    let tex_id = self.texture_ids.get(tex_name).unwrap();
-                                    let bind_group = pipeline.bind_groups.get(tex_id).unwrap();
+                            // Diffuse texture bind group (set to default missing texture if not present on the primitive)
+                            if let Some(tex_name) = &primitive.temp_diffuse {
+                                let tex_id = self.texture_ids.get(tex_name).unwrap();
+                                let bind_group = pipeline.bind_groups.get(tex_id).unwrap();
 
-                                    rpass.set_bind_group(bind_group.0, &bind_group.1, &[]);
-                                } else {
-                                    let tex_id = self.texture_ids.get("missing.jpg").unwrap();
-                                    let bind_group = pipeline.bind_groups.get(tex_id).unwrap();
+                                rpass.set_bind_group(bind_group.0, &bind_group.1, &[]);
+                            } else {
+                                let tex_id = self.texture_ids.get("missing.jpg").unwrap();
+                                let bind_group = pipeline.bind_groups.get(tex_id).unwrap();
 
-                                    rpass.set_bind_group(bind_group.0, &bind_group.1, &[]);
-                                }
-
-                                let cam_bg = pipeline.bind_groups.get(&cam.uuid).unwrap();
-                                rpass.set_bind_group(cam_bg.0, &cam_bg.1, &[]);
+                                rpass.set_bind_group(bind_group.0, &bind_group.1, &[]);
                             }
+
+                            let cam_bg = pipeline.bind_groups.get(&cam.uuid).unwrap();
+                            rpass.set_bind_group(cam_bg.0, &cam_bg.1, &[]);
 
                             if let Some(vb) = self.vertex_buffers.get(&primitive.uuid) {
                                 rpass.set_vertex_buffer(0, vb.get_buffer().slice(..));
+                            }
+
+                            if let Some(instance_buf) = self.instance_buffer.as_ref() {
+                                rpass.set_vertex_buffer(1, instance_buf.slice(..));
                             }
 
                             if let Some(ib) = self.index_buffers.get(&primitive.uuid) {
@@ -323,7 +418,11 @@ impl Sun {
                                 )
                             }
 
-                            rpass.draw_indexed(0..primitive.indices.len() as u32, 0, 0..1);
+                            rpass.draw_indexed(
+                                0..primitive.indices.len() as u32,
+                                0,
+                                0..self.instances.len() as _,
+                            );
                         }
                     }
                 }
@@ -345,6 +444,31 @@ impl Default for Sun {
             ..Default::default()
         });
 
+        let instances = (0..NUM_INSTANCES_PER_ROW)
+            .flat_map(|z| {
+                (0..NUM_INSTANCES_PER_ROW).map(move |x| {
+                    let position = cgmath::Vector3 {
+                        x: x as f32,
+                        y: 0.0,
+                        z: z as f32,
+                    } - INSTANCE_DISPLACEMENT;
+
+                    let rotation = if position.is_zero() {
+                        // this is needed so an object at (0, 0, 0) won't get scaled to zero
+                        // as Quaternions can affect scale if they're not created correctly
+                        cgmath::Quaternion::from_axis_angle(
+                            cgmath::Vector3::unit_z(),
+                            cgmath::Deg(0.0),
+                        )
+                    } else {
+                        cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(45.0))
+                    };
+
+                    Instance { position, rotation }
+                })
+            })
+            .collect::<Vec<_>>();
+
         Self {
             instance: Some(instance),
             adapter: None,
@@ -358,6 +482,8 @@ impl Default for Sun {
             index_buffers: HashMap::new(),
 
             test_texture: None,
+            instances,
+            instance_buffer: None,
 
             texture_ids: HashMap::new(),
             current_camera: None,
@@ -444,10 +570,8 @@ impl App for Sun {
                             .unwrap(),
                         );
 
-                        self.pipelines
-                            .get_mut("basic_shader.wgsl")
-                            .unwrap()
-                            .add_bind_group(
+                        for pipeline in self.pipelines.values_mut() {
+                            pipeline.add_bind_group(
                                 self.device.as_ref().unwrap(),
                                 self.test_texture.as_ref().unwrap().uuid,
                                 "diffuse",
@@ -467,6 +591,7 @@ impl App for Sun {
                                     },
                                 ],
                             );
+                        }
 
                         self.texture_ids.insert(
                             self.test_texture.as_ref().unwrap().name.clone(),
@@ -511,20 +636,16 @@ impl App for Sun {
                                 shader_src: std::str::from_utf8(shader.data.clone().as_slice())
                                     .unwrap()
                                     .to_owned(),
-                                vertex_buffer_layouts: vec![super::primitive::Vertex::desc()],
+                                vertex_buffer_layouts: vec![Vertex::desc(), InstanceRaw::desc()],
                                 topology: if name != "basic_shader.wgsl" {
                                     wgpu::PrimitiveTopology::LineList
                                 } else {
                                     wgpu::PrimitiveTopology::TriangleList
                                 },
-                                bind_group_layout_descs: if name != "basic_shader.wgsl" {
-                                    vec![]
-                                } else {
-                                    vec![
-                                        basic_tex_bg_layout_desc.clone(),
-                                        camera_bg_layout_decs.clone(),
-                                    ]
-                                },
+                                bind_group_layout_descs: vec![
+                                    basic_tex_bg_layout_desc.clone(),
+                                    camera_bg_layout_decs.clone(),
+                                ],
                             };
 
                             self.proxy
