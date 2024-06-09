8413ceefbee5595da738878a5470cd2b47811ce8
Angel Georgiev <210798@buas.nl>
2024-06-09 15:33:38 +0200

    🗨️added id and changed fields to text component

--- src/ecs/text_component.rs
+++ src/ecs/text_component.rs
@@ -1,8 +1,18 @@
-use bevy_ecs::component::Component;
+use bevy_ecs::{component::Component, entity::Entity};
 use cgmath::Vector4;
+
+use super::transform_component::TransformComponent;
+
+// Data sent from Scene to Renderer to render text
+#[derive(Debug, Clone)]
+pub struct RenderTextDesc {
+    pub texts: Vec<(Entity, TextComponent, TransformComponent)>,
+}
 
 #[derive(Debug, Clone, Component)]
 pub struct TextComponent {
+    // Unique identifier (used for buffer generation)
+    pub id: uuid::Uuid,
     // The text to display
     pub text: String,
     // Handle to the font file
@@ -15,17 +25,27 @@ pub struct TextComponent {
     pub kerning: f32,
     // When to break the text into the next line
     pub max_width: f32,
+    // Has the text changed since last buffer update
+    pub changed: bool,
 }
 
 impl Default for TextComponent {
     fn default() -> Self {
         Self {
-            text: Default::default(),
+            text: "TextComponent".to_string(),
             font: "OpenSans-Regular.ttf".to_string(),
             color: Vector4::new(1.0, 1.0, 1.0, 1.0),
-            kerning: Default::default(),
-            line_spacing: Default::default(),
-            max_width: Default::default(),
+            kerning: 0.0,
+            line_spacing: 1.0,
+            max_width: f32::MAX,
+            changed: true,
+            id: uuid::Uuid::new_v4(),
         }
     }
+}
+
+impl TextComponent {
+    // pub fn from_args(args: Vec<&str>) -> Option<Self> {
+    //     for arg in args {}
+    // }
 }
